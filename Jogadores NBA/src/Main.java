f854f7e99b752b25d665df9d89353f3b06da8c1d
Rafael <rafaelradieddine@gmail.com>
2023-03-23 01:04:26 -0300

    implementando variaveis e metodos

--- Jogadores NBA/src/Main.java
+++ Jogadores NBA/src/Main.java
@@ -3,14 +3,105 @@ public class Main {
 
     }
 
-    public class Jogador{
-        int id;
-        String nome;
-        int altura;
-        int peso;
-        String universidade;
-        int anoNascimento;
-        String cidadeNascimento;
-        String estadoNascimento;
+    class  Jogador implements  Cloneable{
+        private int id;
+        private String nome;
+        private int altura;
+        private int peso;
+        private String universidade;
+        private int anoNascimento;
+        private String cidadeNascimento;
+        private String estadoNascimento;
+
+        public void imprimirDados(){
+
+        }
+
+        public Jogador() {
+        }
+
+        public Jogador(int id, String nome, int altura, int peso, String universidade, int anoNascimento, String cidadeNascimento, String estadoNascimento) {
+            this.id = id;
+            this.nome = nome;
+            this.altura = altura;
+            this.peso = peso;
+            this.universidade = universidade;
+            this.anoNascimento = anoNascimento;
+            this.cidadeNascimento = cidadeNascimento;
+            this.estadoNascimento = estadoNascimento;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public int getAltura() {
+            return altura;
+        }
+
+        public void setAltura(int altura) {
+            this.altura = altura;
+        }
+
+        public int getPeso() {
+            return peso;
+        }
+
+        public void setPeso(int peso) {
+            this.peso = peso;
+        }
+
+        public String getUniversidade() {
+            return universidade;
+        }
+
+        public void setUniversidade(String universidade) {
+            this.universidade = universidade;
+        }
+
+        public int getAnoNascimento() {
+            return anoNascimento;
+        }
+
+        public void setAnoNascimento(int anoNascimento) {
+            this.anoNascimento = anoNascimento;
+        }
+
+        public String getCidadeNascimento() {
+            return cidadeNascimento;
+        }
+
+        public void setCidadeNascimento(String cidadeNascimento) {
+            this.cidadeNascimento = cidadeNascimento;
+        }
+
+        public String getEstadoNascimento() {
+            return estadoNascimento;
+        }
+
+        public void setEstadoNascimento(String estadoNascimento) {
+            this.estadoNascimento = estadoNascimento;
+        }
+
+        public Object clone() throws CloneNotSupportedException{
+            return super.clone();
+        }
+
+
+
     }
+
+
 }
