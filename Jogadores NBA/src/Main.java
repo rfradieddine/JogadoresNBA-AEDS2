c924c40768ff6b8705b0a822db7e0b6560aac2fb
Rafael <rafaelradieddine@gmail.com>
2023-04-13 13:16:00 -0300

    atualizando projeto

--- Jogadores NBA/src/Main.java
+++ Jogadores NBA/src/Main.java
@@ -1,167 +1,213 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
 
-class Jogador {
-    private int id, anoNascimento, altura, peso;
-    private String nome, estadoNascimento, universidade, cidadeNascimento;
+class jogador {
+    private int id, altura, peso, anoNascimento;
+    private String nome,universidade, cidadeNascimento,estadoNascimento;
 
-    //Leitura de dados do jogador
-    public void ler(){
-        Scanner sc = new Scanner(System.in);
-
-        this.id = sc.nextInt();
-        this.anoNascimento = sc.nextInt();
-        this.altura = sc.nextInt();
-        this.peso = sc.nextInt();
-        this.nome = sc.nextLine();
-        this.estadoNascimento = sc.nextLine();
-        this.universidade = sc.nextLine();
-        this.cidadeNascimento = sc.nextLine();
-
+    public jogador(){
+        this.id = this.altura = this.peso = this.anoNascimento = 0;
+        this.nome = this.universidade = this.cidadeNascimento = this.estadoNascimento = "";
     }
 
-    //Imprimir dados do jogador
-    public String imprimir() {
-        return toString();
-    }
-
-    //Construtor 1
-    public Jogador() {
-    }
-
-    //Construtor 2
-    public Jogador (int id, String nome, int altura, int peso, String universidade, int anoNascimento,
-                    String cidadeNascimento, String estadoNascimento) {
+    //Construtor que recebe parametros
+    public jogador(final int id, final int altura, final int peso, final int anoNascimento, String nome,
+                   String universidade, String cidadeNascimento, String estadoNascimento) {
         this.id = id;
-        this.nome = nome;
         this.altura = altura;
         this.peso = peso;
-        this.universidade = universidade;
         this.anoNascimento = anoNascimento;
+        this.nome = nome;
+        this.universidade = universidade;
         this.cidadeNascimento = cidadeNascimento;
         this.estadoNascimento = estadoNascimento;
     }
 
-    //Geters e Seters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
+    public void setId( int id) {
         this.id = id;
     }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getAltura() {
-        return altura;
+    public int getId() {
+        return this.id;
     }
 
     public void setAltura(int altura) {
         this.altura = altura;
     }
 
-    public int getPeso() {
-        return peso;
+    public int getAltura() {
+        return this.altura;
     }
 
     public void setPeso(int peso) {
         this.peso = peso;
     }
 
-    public String getUniversidade() {
-        return universidade;
-    }
-
-    public void setUniversidade(String universidade) {
-        this.universidade = universidade;
-    }
-
-    public int getAnoNascimento() {
-        return anoNascimento;
+    public int getPeso() {
+        return this.peso;
     }
 
     public void setAnoNascimento(int anoNascimento) {
         this.anoNascimento = anoNascimento;
     }
 
-    public String getCidadeNascimento() {
-        return cidadeNascimento;
+    public int getAnoNascimento() {
+        return this.anoNascimento;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setUniversidade(String universidade) {
+        this.universidade = universidade;
+    }
+
+    public String getUniversidade() {
+        return this.universidade;
     }
 
     public void setCidadeNascimento(String cidadeNascimento) {
         this.cidadeNascimento = cidadeNascimento;
     }
 
-    public String getEstadoNascimento() {
-        return estadoNascimento;
+    public String getCidadeNascimento() {
+        return this.cidadeNascimento;
+
     }
 
     public void setEstadoNascimento(String estadoNascimento) {
         this.estadoNascimento = estadoNascimento;
     }
 
-    //Metodo toString
-    @Override
+    public String getEstadoNascimento() {
+        return this.estadoNascimento;
+    }
+
+    public void ler() {
+
+    }
+
+    public void imprimir() {
+        System.out.print("["+ getId() + " ## " + getNome() + " ## " + getAltura() + " ## " + getPeso() + " ## " + getAnoNascimento() + " ## " + getUniversidade() + " ## " + getCidadeNascimento()  + " ## " + getEstadoNascimento()  + "]" + "\n" );
+    }
+
+    public jogador clone() {
+        jogador novo = new jogador();
+        novo.id = this.id;
+        novo.nome = this.nome;
+        novo.altura = this.altura;
+        novo.peso = this.peso;
+        novo.universidade = this.universidade;
+        novo.cidadeNascimento = this.cidadeNascimento;
+        novo.estadoNascimento = this.estadoNascimento;
+        return novo;
+    }
+
     public String toString() {
-        return "[" +
-                id +
-                " ## " + nome +
-                " ## " + altura +
-                " ## " + peso +
-                " ## " + anoNascimento +
-                " ## " + universidade +
-                " ## " + cidadeNascimento +
-                " ## " + estadoNascimento +
-                "]";
+        return "[" + id + " ## " + nome + " ## " + altura + " ## " + peso + " ## " + anoNascimento + " ## "
+                + universidade + " ## " + cidadeNascimento + " ## " + estadoNascimento + "]";
     }
 
-    //Metodo clone
-    public Object clone() throws CloneNotSupportedException{
-        return super.clone();
+    public void insert(int i, int array[]) {
+        array[i] = getId();
     }
+
 }
-
 public class Main {
-    public static void main(String[] args) {
-        File file = new File("E:\\Desenvolvimento\\Estudo\\Faculdade\\AEDS2 - Engenharia de Software\\JogadoresNBA-AEDS2\\pub.in");
-        Scanner sc = null;
-        try {
-            sc = new Scanner(file);
-        } catch (FileNotFoundException e) {
-            System.out.println("Erro ao abrir arquivo.");
+
+    public static jogador[] jogador = new jogador[5000];
+    public static int contador = 0;
+    private static PrintStream Arq;
+
+    public static void treatFile(String frase) {
+
+        String[] strResult = new String[8];
+        String str = "";
+
+        for (int i = 0; i <frase.length(); i++) {
+
+            str += frase.charAt(i);
+
+            if ((i < frase.length()-1 && frase.charAt(i) == ',' && frase.charAt(i + 1) == ',') || (i == frase.length() - 1 && frase.charAt(i) == ',')) {
+                str += "nao informado";
+            }
+
+
         }
 
-        // Lê dados dos jogadores
-        Jogador[] jogadores = new Jogador[5000];
-        int numJogadores = 0;
-        String entrada;
-        while(true) {
-            assert sc != null;
-            if ((entrada = sc.nextLine()).equals("FIM")) break;
-            String[] dados = entrada.split(",");
-            Jogador jogador = new Jogador();
-            jogador.setId(Integer.parseInt(dados[0]));
-            jogador.setNome(dados[1]);
-            jogador.setAltura(dados.length > 2 && !dados[2].equals("") ? Integer.parseInt(dados[2]) : 0);
-            jogador.setPeso(dados.length > 3 && !dados[3].equals("") ? Integer.parseInt(dados[3]) : 0);
-            jogador.setUniversidade(dados.length > 4 && !dados[4].equals("") ? dados[4] : "nao informado");
-            jogador.setAnoNascimento(dados.length > 5 && !dados[5].equals("") ? Integer.parseInt(dados[5]) : 0);
-            jogador.setCidadeNascimento(dados.length > 6 && !dados[6].equals("") ? dados[6] : "nao informado");
-            jogador.setEstadoNascimento(dados.length > 7 && !dados[7].equals("") ? dados[7] : "nao informado");
-            jogadores[numJogadores++] = jogador;
+        strResult = str.split(",");
+        criaPlayer(strResult);
+
+    }
+
+    public static void criaPlayer (String[] strResult){
+
+        jogador[contador] = new jogador();
+        jogador[contador].setId(Integer.parseInt(strResult[0]));
+        jogador[contador].setNome(strResult[1]);
+        jogador[contador].setAltura(Integer.parseInt(strResult[2]));
+        jogador[contador].setPeso(Integer.parseInt(strResult[3]));
+        jogador[contador].setUniversidade(strResult[4]);
+        jogador[contador].setAnoNascimento(Integer.parseInt(strResult[5]));
+        jogador[contador].setCidadeNascimento(strResult[6]);
+        jogador[contador].setEstadoNascimento(strResult[7]);
+    }
+
+
+    public static void readFile() {
+        String frase;
+        try{
+            File arqCSV = new File ("/tmp/jogadores.txt");
+            Scanner sc = new Scanner(arqCSV);
+            sc.nextLine();
+            while (sc.hasNextLine()) {
+                frase = sc.nextLine();
+                if (contador > 0)
+                    treatFile(frase);
+                contador++;
+            }
+        }catch (IOException exception){}
+    }
+
+    public static void compara (int entrada, int array[], int n){
+
+        boolean verify = false;
+
+        for (int i=0; i<n && !verify; i++){
+            if (array[i] == (entrada)){
+                verify = true;
+            }
         }
 
-        // Imprime dados dos jogadores
-        for(int i = 0; i < numJogadores; i++) {
-            System.out.println(jogadores[i].imprimir());
+
+        if (verify) System.out.print("SIM\n");
+        else System.out.print("NAO\n");
+    }
+
+    public static boolean isFim(final String s) {
+        return (s.charAt(0) == 'F' && s.charAt(1) == 'I' && s.charAt(2) == 'M');
+    }
+
+    public static void main(final String[] args) {
+        readFile();
+        int i=0;
+        String entradaId;
+        String entrada = MyIO.readLine();
+
+        for (int j = 0; j < Integer.parseInt(entrada); j++) {
+            entradaId = MyIO.readLine();
+            if (isFim(entradaId)){
+                break;
+            }
+            int num = Integer.parseInt(entradaId);
+            System.out.println(jogador[num].toString());
+            i++;
         }
     }
 }
